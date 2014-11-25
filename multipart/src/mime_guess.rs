311c92ede94a657905ec080a38302e9292962b92
Austin Bonander <logicianmlp@gmail.com>
2014-11-25 15:51:30 -0800

    Update mime_guess.rs

--- multipart/src/mime_guess.rs
+++ multipart/src/mime_guess.rs
@@ -26,6 +26,8 @@ pub fn guess_mime_type_filename(filename: &str) -> Mime {
 local_data_key!(mime_types_cache: RefCell<HashMap<String, Mime>>)
 
 /// Get the MIME type associated with a file extension
+/// If there is no association for the extension,
+/// `application/octet-stream` is assumed.
 pub fn get_mime_type(ext: &str) -> Mime {
     if ext.is_empty() { return octet_stream(); }
 
