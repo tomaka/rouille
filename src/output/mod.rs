6ab97516db4558e7371ad0d4c2fa75119979bfaf
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-07-16 08:08:45 +0200

    Add RedirectOutput

--- src/output/mod.rs
+++ src/output/mod.rs
@@ -2,9 +2,11 @@ use hyper::server::response::Response as HyperResponse;
 
 pub use self::json::JsonOutput;
 pub use self::plain_text::PlainTextOutput;
+pub use self::redirect::RedirectOutput;
 
 pub mod json;
 pub mod plain_text;
+pub mod redirect;
 
 /// Objects that can serve as a response to the request.
 pub trait Output {
---
+++ src/output/redirect.rs
@@ -0,0 +1,44 @@
+use hyper::server::response::Response as HyperResponse;
+use super::Output;
+
+/// A response that redirects the user to another URL.
+pub struct RedirectOutput {
+    /// The URL to redirect to.
+    pub location: String,
+    /// The status code to use.
+    pub status_code: u16,
+}
+
+impl RedirectOutput {
+    /// Builds a `RedirectOutput`.
+    ///
+    /// ```rust
+    /// use rouille::output::RedirectOutput;
+    /// let output = RedirectOutput::new("/hello", 302);
+    /// ```
+    pub fn new<S>(location: S, status_code: u16) -> RedirectOutput where S: Into<String> {
+        RedirectOutput {
+            location: location.into(),
+            status_code: status_code,
+        }
+    }
+}
+
+impl Output for RedirectOutput {
+    fn send(self, mut response: HyperResponse) {
+        use hyper::header::Location;
+        use hyper::status::StatusCode;
+
+        // no parse method?
+        *response.status_mut() = match self.status_code {
+            301 => StatusCode::MovedPermanently,
+            302 => StatusCode::Found,
+            303 => StatusCode::SeeOther,
+            c => StatusCode::Unregistered(c),
+        };
+
+        response.headers_mut().set(Location(self.location));
+
+        let _ = response.send(&[]);
+    }
+}
