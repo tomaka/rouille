7556ca191ab083a6cce026e0c5a236a9f80019a7
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-09-09 11:34:20 +0200

    Fix the simple_form example

--- examples/simple_form.rs
+++ examples/simple_form.rs
@@ -12,24 +12,24 @@ fn main() {
     rouille::start_server("localhost:8000", move |request| {
         let _entry = rouille::LogEntry::start(io::stdout(), request);
 
-        let response = router!(request,
-            (GET) (/) => (|| {
+        let response = (|| router!(request,
+            (GET) (/) => {
                 let mut output = Vec::new();
                 form.render_data(&mut output, &mustache::Data::Bool(false));
                 Ok(rouille::Response::html(output))
-            }),
+            },
 
-            (POST) (/submit) => (|| {
+            (POST) (/submit) => {
                 let data: FormData = try!(rouille::input::get_post_input(request));
                 println!("{:?}", data);
 
                 let mut output = Vec::new();
                 form_success.render(&mut output, &data);
                 Ok(rouille::Response::html(output))
-            }),
+            },
 
-            _ => || Err(rouille::RouteError::NoRouteFound)
-        );
+            _ => Err(rouille::RouteError::NoRouteFound)
+        ))();
 
         response.unwrap_or_else(|err| rouille::Response::from_error(&err))
     });
