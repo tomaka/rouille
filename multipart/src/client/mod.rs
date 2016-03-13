a86caa9921f45352894525f66c0d7379b8c43e4c
Austin Bonander <austin.bonander@gmail.com>
2016-03-12 21:50:42 -0800

    Change license to Apache-2.0/MIT

--- multipart/src/client/mod.rs
+++ multipart/src/client/mod.rs
@@ -1,3 +1,9 @@
+// Copyright 2016 `multipart` Crate Developers
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
 //! The client-side abstraction for multipart requests. Enabled with the `client` feature (on by
 //! default).
 //!
