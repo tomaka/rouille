eee1345295547b605357f5c7dca02db1f5bbdfd7
Austin Bonander <austin.bonander@gmail.com>
2016-03-13 00:05:44 -0800

    Change docs deploy process

--- multipart/deploy-docs.sh
+++ multipart/deploy-docs.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
+
+#Decrypt RSA key
+mkdir -p ~/.ssh
+openssl aes-256-cbc -K $encrypted_0a6446eb3ae3_key -iv $encrypted_0a6446eb3ae3_key -in id_rsa.enc -out ~/.ssh/id_rsa -d
+chmod 600 ~/.ssh/id_rsa
+
 cargo doc -v --no-deps
-cp -R target/doc doc
-curl http://www.rust-ci.org/artifacts/put?t=$RUSTCI_TOKEN | sh
-rm -r doc
+
+git config user.name "multipart doc upload"
+git config user.email "nobody@example.com"
+
+git branch -df gh-pages
+git checkout --orphan gh-pages
+
+git clean -d -x -f -e target/doc
+
+cp -R target/doc .
+rm -rf target
+
+git add -A
+
+git commit -qm "Documentation for ${TRAVIS_TAG}"
+git push -f origin gh-pages
