1c1a81e41ec7ad9279aed5294aff69cbfbf46aeb
Austin Bonander <austin.bonander@gmail.com>
2016-03-13 03:17:24 -0700

    Fix deploy-docs

--- multipart/deploy-docs.sh
+++ multipart/deploy-docs.sh
@@ -3,19 +3,16 @@
 
 #Decrypt RSA key
 mkdir -p ~/.ssh
-openssl aes-256-cbc -K $encrypted_0a6446eb3ae3_key -iv $encrypted_0a6446eb3ae3_key -in id_rsa.enc -out ~/.ssh/id_rsa -d
+openssl aes-256-cbc -K $encrypted_9e7566e23d77_key -iv $encrypted_9e7566e23d77_iv -in id_rsa.enc -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
-
-cargo doc -v --no-deps
 
 git config user.name "multipart doc upload"
 git config user.email "nobody@example.com"
 
-git branch -df gh-pages
 git checkout --orphan gh-pages
 
 git reset
-git clean -d -x -f -e target/doc
+git clean -d -x -f -e target
 
 cp -R target/doc .
 rm -rf target
