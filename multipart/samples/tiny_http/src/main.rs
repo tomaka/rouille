634c11d71b7ed8d4ad4bba8e118ae3d7a9c23f96
White-Oak <lpzhelud@gmail.com>
2016-03-19 07:11:46 +0300

    Added a response for tiny_http sample

--- multipart/samples/tiny_http/src/main.rs
+++ multipart/samples/tiny_http/src/main.rs
@@ -2,27 +2,35 @@ extern crate tiny_http;
 extern crate multipart;
 
 use multipart::server::{Multipart, Entries, SaveResult};
+use tiny_http::{Response, StatusCode, Request};
 fn main() {
     // Starting a server on `localhost:80`
     let server = tiny_http::Server::http("localhost:80").unwrap();
     loop {
         // This blocks until the next request is received
         let mut request = server.recv().unwrap();
+        process_multipart(&mut request).unwrap();
 
-        // Getting a multipart reader wrapper
-        let mut multipart = Multipart::from_request(&mut request).unwrap();
-        // Fetching all data and processing it
-        match multipart.save_all() {
-            SaveResult::Full(entries) => process_entries(entries).unwrap(),
-            SaveResult::Partial(entries, error) => {
-                process_entries(entries).unwrap();
-                panic!("{:?}", error)
-            },
-            SaveResult::Error(error) => panic!("{:?}", error)
-        }
+        // Answering with an HTTP OK and a string
+        let response_string = "Multipart data is received!".as_bytes();
+        let response = Response::new(StatusCode(200), vec![], response_string, Some(response_string.len()), None);
+        request.respond(response).unwrap();
     }
 }
 
+fn process_multipart(request: &mut Request) -> Result<(), Error> {
+    // Getting a multipart reader wrapper
+    let mut multipart = Multipart::from_request(request).unwrap();
+    // Fetching all data and processing it
+    match multipart.save_all() {
+        SaveResult::Full(entries) => process_entries(entries),
+        SaveResult::Partial(entries, error) => {
+            try!(process_entries(entries));
+            Err(error)
+        },
+        SaveResult::Error(error) => Err(error)
+    }
+}
 use std::fs::File;
 use std::io::{Error, Read};
 fn process_entries(entries: Entries) -> Result<(), Error> {
