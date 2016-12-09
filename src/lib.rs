14d8b0f248027b0a25d8d11b003a9c7cee4b05ff
Chris Wong <lambda.fairy@gmail.com>
2016-12-09 16:06:37 +1300

    Rename `secure` to `is_secure`

--- src/lib.rs
+++ src/lib.rs
@@ -428,7 +428,7 @@ impl Request {
     /// use rouille::{Request, Response};
     ///
     /// fn handle(request: &Request) -> Response {
-    ///     if !request.secure() {
+    ///     if !request.is_secure() {
     ///         return Response::redirect(&format!("https://example.com"));
     ///     }
     ///
@@ -437,7 +437,7 @@ impl Request {
     /// }
     /// ```
     #[inline]
-    pub fn secure(&self) -> bool {
+    pub fn is_secure(&self) -> bool {
         self.https
     }
 
