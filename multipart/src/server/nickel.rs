fa43ba0dd258827c01703eacc03258305a90c494
Austin Bonander <austin.bonander@gmail.com>
2017-03-20 16:21:22 -0700

    Add deprecation note to `nickel` module and a warning log message in the impl

--- multipart/src/server/nickel.rs
+++ multipart/src/server/nickel.rs
@@ -2,6 +2,11 @@
 //! (optional, enables `hyper` feature).
 //!
 //! Not shown here: [`impl HttpRequest for &mut nickel::Request`](../trait.HttpRequest.html#implementors).
+//!
+//! **Note**: in-crate integration for Nickel is deprecated and will be removed in 0.11.0;
+//! integration will be provided in the
+//! [`multipart-nickel`](https://crates.io/crates/multipart-nickel)
+//! crate for the foreseeable future.
 #![deprecated(since = "0.10.2", note = "Nickel integration has moved to the `multipart-nickel`
                                         crate; in-crate integration will be removed in 0.11.0")]
 use nickel::Request as NickelRequest;
@@ -20,6 +25,9 @@ impl<'r, 'mw, 'server, D: 'mw> HttpRequest for &'r mut NickelRequest<'mw, 'serve
     }
 
     fn body(self) -> Self::Body {
+        warn!("In-crate Nickel integration is deprecated and will be removed in 0.11.0; \
+               please consider switching to the `multipart-nickel` crate to avoid breakage.");
+
         &mut self.origin
     }
 }
