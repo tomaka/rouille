e60417f363359d81920e528d6fdebd242e460212
Trevor Gross <tmgross@umich.edu>
2025-06-17 18:14:23 +0000

    ci: Include rouille-multipart in tests

--- rouille-multipart/examples/tiny_http.rs
+++ rouille-multipart/examples/tiny_http.rs
@@ -1,5 +1,5 @@
-use multipart::mock::StdoutTee;
-use multipart::server::{Entries, Multipart, SaveResult};
+use rouille_multipart::mock::StdoutTee;
+use rouille_multipart::server::{Entries, Multipart, SaveResult};
 use std::io::{self, Cursor, Write};
 use tiny_http::{Request, Response, StatusCode};
 
